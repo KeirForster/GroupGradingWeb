2f84f72a5e2d2d64e518dfafd8616071746ba7ec
Keir Forster <keirforster@gmail.com>
2018-11-28 19:15:52 -0800

    Add angular-bootstrap-md dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+// angular modules
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
+// app modules
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
+
+
+// components
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
     declarations: [AppComponent, AuthComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        MDBBootstrapModule.forRoot(),
+        AppRoutingModule
+    ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule {}
